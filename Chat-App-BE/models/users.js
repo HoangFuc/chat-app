5bc8b5494231769e890e446439dba59fed22350b
Phuc Nguyen <phuc.nguyen@nexpando.com>
2024-03-08 17:04:58 +0700

    add field email

--- Chat-App-BE/models/users.js
+++ Chat-App-BE/models/users.js
@@ -6,6 +6,10 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+    email: {
+      type: String,
+      unique: true
+    },
     password: {
       type: String,
       required: true,
