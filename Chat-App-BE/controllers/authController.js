fa2789d3fb4e5c41c0ba2769f25062b525885d9c
Trà Thanh Tú <131364996+TraThanhTu@users.noreply.github.com>
2024-03-10 13:26:49 +0700

    Update authController.js

--- Chat-App-BE/controllers/authController.js
+++ Chat-App-BE/controllers/authController.js
@@ -21,9 +21,7 @@ exports.signup = async (req, res) => {
 };
 
 exports.signin = async (req, res) => {
-  console.log('-------------------------', req.body);
   const { username, password } = req.body;
-
   try {
     const data = await userModel.findOne({
       username: username,
