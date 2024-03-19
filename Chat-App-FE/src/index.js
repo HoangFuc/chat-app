a97b18af7f66b75ac87cdd71614a476a4cf6a5c1
Trà Thanh Tú <131364996+TraThanhTu@users.noreply.github.com>
2024-03-19 16:46:04 +0700

    Update index.js

--- Chat-App-FE/src/index.js
+++ Chat-App-FE/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
